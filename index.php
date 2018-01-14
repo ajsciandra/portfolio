55367e48beab707aa0a89692cde66d24d769db33
ajsciandra <andrewsciandra@comcast.net>
2018-01-13 20:47:45 -0800

    made the header

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Andrew Sciandra Personal Portfolio</title>
+  <meta charset="utf-8">
+	<link rel="stylesheet" href="port.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+  <?php include("includes/header.html");?>
+  <div class = "container-fluid" id="about-me">
+    <div class = beign-text">
+      <h4 id="start-text"> Software Developer with Practical Experience in AGILE Development Plan. Knowledgeable with C++, Python, Java</h4>
+    </div>
+    <hr>
+  </div>
+
+
+</body>
+</html>
