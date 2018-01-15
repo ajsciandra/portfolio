e6703e2d56bc5d3f34b2e48ea91de1f0cec46f7f
ajsciandra <andrewsciandra@comcast.net>
2018-01-14 20:35:03 -0800

    working on past project for portfolio

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Andrew Sciandra Personal Portfolio</title>
   <meta charset="utf-8">
-	<link rel="stylesheet" href="port.css">
+	<link rel="stylesheet" href="includes/css/port.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -15,9 +15,52 @@
   <?php include("includes/header.html");?>
   <div class = "container-fluid" id="about-me">
     <div class = beign-text">
-      <h4 id="start-text"> Software Developer with Practical Experience in AGILE Development Plan. Knowledgeable with C++, Python, Java</h4>
+      <h4 class="start-text"> Software Developer with Practical Experience in AGILE Development Plan. Knowledgeable with C++, Python, Java</h4>
     </div>
-    <hr>
+    <hr id="about-hr">
+    <h4 class = "start-text">Software Engineer - Web Developer - Computer Science Student</h4>
+    <img src = "includes/photos/cubs.jpeg" class = "my-photo">
+  </div>
+
+  <!----------------------End About Me---------------------->
+
+
+  <div class = "container-fluid" id="my-port">
+    <h4 class = "start-text" id="start-port"> Portfolio </h4>
+    <hr id = "port-hr">
+    <div class = "thumbnail">
+      <a href= "#">
+        <img src = "includes/photos/connect-four.jpeg" class = "img-responsive">
+        <div class = "caption">
+          <p>W.I.P - Connect Four in Python</p>
+        </div>
+      </a>
+    </div>
+
+    <div class = thumbnail>
+      <a href = "#">
+      <img src = "includes/photos/expKit.jpeg" class = "img-responsive">
+        <div class = "caption">
+          <p>Exploding Kittens in C++</p>
+        </div>
+      </a>
+    </div>
+
+    <div class = "thumbnail">
+      <a href="#">
+      <img src="includes/photos/garden.jpeg" class = "img-responsive">
+        <div class = "caption">
+          <p>Garden Simulation in C++</p>
+        </div>
+      </a>
+    </div>
+
+  </div>
+
+  <!----------------------End My Portfolio ---------------------->
+
+  <div class = "container-fluid" id="contact-me">
+
   </div>
 
 
